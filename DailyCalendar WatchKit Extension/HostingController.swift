1b3909cbf7d7abadc9d6173f864104bc0b37c528
i.fomenko <ivanfomenkowork@gmail.com>
2020-04-12 20:42:43 +0300

    Initial Commit

---
+++ DailyCalendar WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  DailyCalendar WatchKit Extension
+//
+//  Created by Ivan Fomenko on 12.04.2020.
+//  Copyright © 2020 com.ivanfomenko.social. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
